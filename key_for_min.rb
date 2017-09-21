1faf8ac0413567f8bc08342f6d076e2ed2b03544
Chris Nunez <cenunez47@gmail.com>
2017-09-21 01:07:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |x, y|
     hold << y
   end
-    name_hash.collect |x, y|
+    name_hash.collect do |x, y|
       if y <= hash.all?
         x
       end
